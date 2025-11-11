21d195fa413c15337f2b806e927aa3ec72b1b692
jihyun0121 <gheun098@gmail.com>
2025-11-11 09:36:36 +0900

    User 엔티티 클래스에 JSON 속성 추가

--- backend/src/main/java/form/backend/entity/User.java
+++ backend/src/main/java/form/backend/entity/User.java
@@ -3,6 +3,9 @@ package form.backend.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.LocalDateTime;
 
 @Entity
@@ -15,6 +18,7 @@ import java.time.LocalDateTime;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("user_id")
     private Long userId;
 
     @Column(nullable = false, unique = true)
@@ -24,5 +28,6 @@ public class User {
     private String password;
 
     @CreationTimestamp
+    @JsonProperty("created_at")
     private LocalDateTime createdAt;
 }
