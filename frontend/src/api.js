cbe2e7a8c04661efd6673dfdf82f9bd9d2d627d1
jihyun0121 <gheun098@gmail.com>
2025-11-14 11:04:45 +0900

    섹션 관련 api 연동 추가

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -25,37 +25,37 @@ api.interceptors.response.use(
     }
 );
 
-export const aa = () => api.bb(`/users/signup`);
-export const aa = () => api.bb(`/users/login`);
-export const aa = () => api.bb(`/users/${userId}`);
+export const signup = () => api.POST(`/users/signup`);
+export const login = () => api.POST(`/users/login`);
+export const getProfile = (userId) => api.GET(`/users/${userId}`);
 
-export const aa = () => api.bb(`/forms`);
-export const aa = () => api.bb(`/forms`);
-export const aa = () => api.bb(`/forms/${formId}`);
-export const aa = () => api.bb(`/forms/user/${userId}`);
-export const aa = () => api.bb(`/forms/${formId}`);
-export const aa = () => api.bb(`/forms/${formId}`);
-export const aa = () => api.bb(`/forms/${formId}/structure`);
+export const createForm = () => api.POST(`/forms`);
+export const getAllForms = () => api.GET(`/forms`);
+export const getFormById = (formId) => api.GET(`/forms/${formId}`);
+export const getFormByUserId = (userId) => api.GET(`/forms/user/${userId}`);
+export const updateForm = (formId) => api.PUT(`/forms/${formId}`);
+export const deleteForm = (formId) => api.DELETE(`/forms/${formId}`);
+export const getFormStructure = (formId) => api.GET(`/forms/${formId}/structure`);
 
-export const aa = () => api.bb(`/forms/${formId}/sections`);
-export const aa = () => api.bb(`/sections/${sectionId}`);
-export const aa = () => api.bb(`/sections/${sectionId}`);
-export const aa = () => api.bb(`/forms/${formId}/sections`);
+export const addSection = (formId) => api.POST(`/forms/${formId}/sections`);
+export const updateSection = (sectionId) => api.PUT(`/sections/${sectionId}`);
+export const deleteSection = (sectionId) => api.DELETE(`/sections/${sectionId}`);
+export const getAllSection = (formId) => api.GET(`/forms/${formId}/sections`);
 
-export const aa = () => api.bb(`/forms/${formId}/question`);
-export const aa = () => api.bb(`/forms/${formId}/questions`);
-export const aa = () => api.bb(`/questions/${questionId}`);
-export const aa = () => api.bb(`/questions/${questionId}`);
+export const addQuestion = (formId) => api.POST(`/forms/${formId}/question`);
+export const getQuestionByFormId = (formId) => api.GET(`/forms/${formId}/questions`);
+export const updateQuestion = (questionId) => api.PUT(`/questions/${questionId}`);
+export const deleteQuestion = (questionId) => api.DELETE(`/questions/${questionId}`);
 
-export const aa = () => api.bb(`/forms/${formId}/responses`);
-export const aa = () => api.bb(`/forms/${formId}/responses`);
-export const aa = () => api.bb(`/responses/${responseId}`);
-export const aa = () => api.bb(`/users/${userId}/responses`);
-export const aa = () => api.bb(`/responses/${responseId}`);
+export const createResponse = (formId) => api.POST(`/forms/${formId}/responses`);
+export const getResponseByFormId = (formId) => api.GET(`/forms/${formId}/responses`);
+export const getResponseById = (responseId) => api.GET(`/responses/${responseId}`);
+export const getResponseByUserId = (userId) => api.GET(`/users/${userId}/responses`);
+export const deleteResponse = (responseId) => api.DELETE(`/responses/${responseId}`);
 
-export const aa = () => api.bb(`/responses/${responseId}/answers`);
-export const aa = () => api.bb(`/questions/${questionId}/answers`);
-export const aa = () => api.bb(`/responses/${responseId}/answers`);
-export const aa = () => api.bb(`/responses/${responseId}/answers`);
+export const addAnswers = (responseId) => api.POST(`/responses/${responseId}/answers`);
+export const getAnswerByQuestionId = (questionId) => api.GET(`/questions/${questionId}/answers`);
+export const getAnswerByResponseId = (responseId) => api.GET(`/responses/${responseId}/answers`);
+export const updateAnswers = (responseId) => api.PUT(`/responses/${responseId}/answers`);
 
 export default api;
